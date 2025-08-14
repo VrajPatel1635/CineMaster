ec6d58559797cc77864bf84f058d73521c7b7f44
Vraj Patel <vrajrpatel6261@gmail.com>
2025-08-14 15:06:54 +0530

    final commit

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -24,8 +24,7 @@ export const registerUser = async (req, res) => {
     const exists = await User.findOne({ email });
     if (exists) return res.status(400).json({ message: 'User already exists' });
 
-    const hashed = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashed });
+  const newUser = await User.create({ email, password });
 
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.status(201).json({ user: newUser, token });
